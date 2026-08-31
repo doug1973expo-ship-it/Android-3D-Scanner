77db73b3227a31322792a9ae6dded5d7ac6c0c26
doug1973expo-ship-it <doug1973expo@gmail.com>
2026-08-31 22:14:52 +0100

    Bump camera preview version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "uk.co.pocket3d.scanner.motorola"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     buildFeatures { viewBinding = true }
     signingConfigs {
