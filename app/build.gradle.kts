36ef6b732194741546b5573bc45b76d9886afa0b
doug1973expo-ship-it <doug1973expo@gmail.com>
2026-08-31 22:29:41 +0100

    Bump camera rendering fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "uk.co.pocket3d.scanner.edge20"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
     buildFeatures { viewBinding = true }
     signingConfigs {
